4be2761351cc408b0040a1bcfd43d6be02e882c3
Davi Taveira <davitaveira@gmail.com>
2023-03-01 21:42:00 -0300

    aula-01

---
+++ 2023-ADS-POOI-producao/src/Aula01/Ex01.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Aula01;
+
+/**
+ *
+ * @author Davi
+ */
+import java.util.Scanner;
+
+public class Ex01 {
+    
+    public static void main(String[] args) {
+        /*
+        String nome;
+        double altura;
+        double distancia_cm;
+        int idade;
+        double volume_lt;
+        int qtdfilhos;
+        boolean arquivo;
+        
+        nome="davi";
+        altura=1.89;
+        distancia_cm = 3.18;
+        qtdfilhos = 5;
+        arquivo = false;
+        */
+        
+        String nome;
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Informe o nome: ");
+        nome = entrada.next();
+        System.out.println("O nome digitado é: "+nome);
+                
+    }
+    
+}
