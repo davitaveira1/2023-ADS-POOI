c5fab9c24b7921629be74de2b905db99b39e6d0b
Davi Taveira <davitaveira@gmail.com>
2023-05-03 20:22:39 -0300

    exercicio-sala

---
+++ 2023-ADS-POOI-producao/src/Aula08/Principal.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Aula08;
+
+/**
+ *
+ * @author Davi
+ */
+public class Principal {
+    
+    public static void main(String[] args) {
+        Professor p1 = new Professor("Davi");
+        Aluno a1 = new Aluno("Lucas");
+        
+        Turma poo = new Turma("ADS", p1, "103", 10);
+        
+    }
+    
+}
---
+++ 2023-ADS-POOI-producao/src/Aula08/Turma.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Aula08;
+
+/**
+ *
+ * @author Davi
+ */
+public class Turma {
+
+    String curso;
+    Professor professor;
+    String sala;
+    int Max_alunos;
+    Aluno[] alunos;
+    int qtdAlunos;
+
+    public Turma(String curso, Professor professor, String sala, int Max_alunos) {
+        this.curso = curso;
+        this.professor = professor;
+        this.sala = sala;
+        this.Max_alunos = Max_alunos;
+        alunos = new Aluno[Max_alunos];
+    }
+
+    void adicionarAluno(Aluno a) {
+        if (qtdAlunos > Max_alunos) {
+            System.out.println("Turma lotada! Aluno não adicionado!");
+        } else {
+
+            for (int i = 0; i < alunos.length; i++) {
+                if (alunos[i] == null) {
+                    alunos[i] = a;
+                }
+            }
+            System.out.println("Aluno inserido com sucesso!");
+
+        }
+    }
+
+    void imprimir() {
+        for (int i = 0; i < alunos.length; i++) {
+            System.out.println("Aluno: "+alunos[i].getNome());
+        }
+    }
+
+}
