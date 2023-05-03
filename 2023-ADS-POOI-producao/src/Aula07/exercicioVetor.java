0cc8fcb364d827a23e5a52407bfac64b9872e045
Davi Taveira <davitaveira@gmail.com>
2023-05-03 19:54:00 -0300

    aula-exercicio-sala

--- 2023-ADS-POOI-producao/src/Aula07/exercicioVetor.java
+++ 2023-ADS-POOI-producao/src/Aula07/exercicioVetor.java
@@ -27,19 +27,96 @@ public class exercicioVetor {
             soma = soma + vet[i];
         }
         media = soma / vet.length;
-        System.out.printf("Média aritmética: %.1f\n",media);
+        System.out.printf("Média aritmética: %.1f\n", media);
 
-        soma=0;
+        soma = 0;
         double mediaPonderada;
-        double somaPesos=0;
+        double somaPesos = 0;
         int[] pesos = {4, 3, 2, 5, 4, 6, 6, 7, 8, 2};
 
         for (int i = 0; i < vet.length; i++) {
-            soma = soma + (vet[i]*pesos[i]);
+            soma = soma + (vet[i] * pesos[i]);
             somaPesos = somaPesos + pesos[i];
         }
-        mediaPonderada = soma/somaPesos;
-        System.out.printf("Média ponderada: %.1f\n",mediaPonderada);
+        mediaPonderada = soma / somaPesos;
+        System.out.printf("Média ponderada: %.1f\n", mediaPonderada);
+
+    }
+
+    void buscar(double valor) {
+        int flag = 0;
+        for (int i = 0; i < vet.length; i++) {
+            if (vet[i] == valor) {
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 1) {
+            System.out.println("Encontrou o valor " + valor);
+        } else {
+            System.out.println("Não encontrou o valor " + valor);
+        }
+    }
+
+    void excluir(double valor) {
+        int flag = 0;
+        for (int i = 0; i < vet.length; i++) {
+            if (vet[i] == valor) {
+                vet[i] = -1;
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 1) {
+            System.out.println("O " + valor + " foi excluído!");
+        } else {
+            System.out.println("O " + valor + " não foi encontrado!");
+        }
+    }
+
+    void excluirPorPosicao(int pos) {
+        int flag = 0;
+        for (int i = 0; i < vet.length; i++) {
+            if (i == pos) {
+                vet[i] = -1;
+                flag = 1;
+                break;
+            }
+        }
+
+        if (flag == 1) {
+            System.out.println("Posição encontrada e excluída! ");
+        } else {
+            System.out.println("Posição não foi encontrada");
+        }
+
+    }
+
+    void qtdPosicoesDisponiveis() {
+        int qtd = 0;
+        for (int i = 0; i < vet.length; i++) {
+            if (vet[i] == -1) {
+                qtd++;
+            }
+        }
+        System.out.println("Posições disponíveis: " + qtd);
+    }
+
+    void inserir(double valor) {
+        for (int i = 0; i < vet.length; i++) {
+            if (vet[i] == -1) {
+                vet[i] = valor;
+                break;
+            }
+        }
+    }
+
+    void inserirPorPosicao(int pos, double valor) {
+        if (pos >= 0 && pos < vet.length) {
+            vet[pos] = valor;
+        }else{
+            System.out.println("Posição inválida! Valor não inserido!");
+        }
 
     }
 
@@ -47,6 +124,25 @@ public class exercicioVetor {
         exercicioVetor obj = new exercicioVetor();
         obj.imprimir();
         obj.medias();
+        obj.buscar(50);
+        obj.buscar(21.9);
+        obj.excluir(12.1);
+        obj.imprimir();
+        obj.excluir(100);
+        obj.excluirPorPosicao(0);
+        obj.excluirPorPosicao(2);
+        obj.excluirPorPosicao(4);
+        obj.excluirPorPosicao(6);
+        obj.imprimir();
+        obj.qtdPosicoesDisponiveis();
+        obj.inserir(10);
+        obj.inserir(20);
+        obj.inserir(30);
+        obj.inserir(40);
+        obj.imprimir();
+        obj.inserirPorPosicao(9, 50);
+        obj.imprimir();
+        
     }
 
 }
