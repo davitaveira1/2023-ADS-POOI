1c77c430c3934b8d57eb539b1616dad1562d51a9
Davi Taveira <davitaveira@gmail.com>
2023-06-28 20:10:09 -0300

    aula-16

---
+++ 2023-ADS-POOI-producao/src/Aula16/Principal1.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Aula16;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Davi
+ */
+public class Principal1 {
+
+    public static void main(String[] args) {
+
+        File file = new File("c:\\poo\\entrada.txt");
+        Scanner sc = null;
+
+        try {
+            sc = new Scanner(file);
+            
+            while(sc.hasNextLine()){
+                System.out.println(sc.nextLine());
+            }
+            
+        } catch (IOException e) {
+            System.out.println("Erro: "+e.getMessage());
+        } finally{
+            if(sc!=null){
+                sc.close();
+            }            
+        }
+
+    }
+
+}
